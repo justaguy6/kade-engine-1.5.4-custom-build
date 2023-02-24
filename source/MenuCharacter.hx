8192aafd46b8b5bb4ea6942614ce46470b5c7dfa
Random people <98900217+justaguy6@users.noreply.github.com>
2023-02-24 11:53:57 -0300

    Update MenuCharacter.hx

--- source/MenuCharacter.hx
+++ source/MenuCharacter.hx
@@ -68,36 +68,7 @@ class MenuCharacter extends FlxSprite
 		updateHitbox();
 	}
 
-	var characterPath:String = 'images/menucharacters/' + character + '.json';
-        var rawJson = null;
-
-	#if MODS_ALLOWED
-	var path:String = Paths.modFolders(characterPath);
-	if (!FileSystem.exists(path)) 
-        {
-		path = SUtil.getStorageDirectory() + Paths.getPreloadPath(characterPath);
-        }
-
-	if(!FileSystem.exists(path))
-        {
-		path = SUtil.getStorageDirectory() + Paths.getPreloadPath('images/menucharacters/' + DEFAULT_CHARACTER + '.json');
-
-	rawJson = File.getContent(path);
-        }
-	#else
-	var path:String = Paths.getPreloadPath(characterPath);
-	if(!Assets.exists(path)) 
-        {
-          	path = Paths.getPreloadPath('images/menucharacters/' + DEFAULT_CHARACTER + '.json');
-	}
-	rawJson = Assets.getText(path);
-	#end
-		
-	var charFile:MenuCharacterFile = cast Json.parse(rawJson);
-	frames = Paths.getSparrowAtlas('menucharacters/' + charFile.image);
-	animation.addByPrefix('idle', charFile.idle_anim, 24);	
 	
-
 	public function setCharacter(character:String):Void
 	{
 		if (character == '')
