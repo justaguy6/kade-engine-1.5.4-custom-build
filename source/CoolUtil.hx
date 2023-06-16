632316a43f54ce9cace63b4aec6f06c885a386c8
Random people <98900217+justaguy6@users.noreply.github.com>
2023-06-16 18:03:34 -0300

    Update CoolUtil.hx

--- source/CoolUtil.hx
+++ source/CoolUtil.hx
@@ -57,4 +57,12 @@ class CoolUtil
 		}
 		return dumbArray;
 	}
+
+	public static function browserLoad(site:String) {
+		#if linux
+		Sys.command('/usr/bin/xdg-open', [site]);
+		#else
+		FlxG.openURL(site);
+		#end
+	}
 }
